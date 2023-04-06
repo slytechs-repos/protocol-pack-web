3142040c5f3cf6f66c4b5a977924991aed8c8f6a
Mark Bednarczyk <mark@slytechs.com>
2023-04-05 21:49:49 -0400

    Initial import

---
+++ src/main/java/com/slytechs/jnet/protocol/web/constants/WebConstants.java
@@ -0,0 +1,39 @@
+/*
+ * Sly Technologies Free License
+ * 
+ * Copyright 2023 Sly Technologies Inc.
+ *
+ * Licensed under the Sly Technologies Free License (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.slytechs.com/free-license-text
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.slytechs.jnet.protocol.web.constants;
+
+/**
+ * @author Sly Technologies Inc
+ * @author repos@slytechs.com
+ * @author Mark Bednarczyk
+ *
+ */
+public final class WebConstants {
+	
+	/* @formatter:off - http constants */
+	/* @formatter:on - http constants */
+	
+	/* @formatter:off - html constants */
+	/* @formatter:on - html constants */
+
+	
+
+	private WebConstants() {
+	}
+
+}
